fc24150f194d5653d2535159477c10b725f4f7c1
Eduardo Dominikus <eduardodominikus@hotmail.com>
2026-05-28 13:34:24 +0100

    restore: embed do carrossel App Group (sem secao duplicada na landing)

--- app/layout.tsx
+++ app/layout.tsx
@@ -200,6 +200,12 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             {children}
           </main>
         </AuthProvider>
+        <Script
+          src="https://appgroupbrasil.com.br/embed/app-group-banner.js"
+          strategy="afterInteractive"
+          data-color="#057321"
+          data-routes="/"
+        />
       </body>
     </html>
   );
